bc5e80185480626da413ef413daf50bad5bb10a3
Evgeny Stepanov <evgenos@list.ru>
2020-07-25 23:20:08 +0300

    refactoring EditMenu

--- src/components/Main/EditMenu.js
+++ src/components/Main/EditMenu.js
@@ -2,39 +2,33 @@ import React from 'react';
 import { StoreContext } from '../../service/store';
 import './EditMenu.scss';
 
-export default function EditMenu(props) {
+export default function EditMenu({id}) {
   const {
     state: [state, setState],
   } = React.useContext(StoreContext);
 
   function editCard() {
-    props.setEdit(!props.edit);
-    props.setToggleMenu(!props.toggleMenu);
+    // setEdit(!props.edit);
+    // setToggleMenu(!props.toggleMenu);
   }
 
   function deleteCard(id) {
     setState(state.slice().filter((i) => i.id !== id));
   }
 
-  if (props.toggleMenu) {
-    return (
-      <div className="EditMenu">
-        <button
-          autoFocus
-          className="button EditMenu__edit"
-          onClick={editCard}
-        >
-          <i className="EditMenu__icon fas fa-pencil-alt"></i>Редактировать
-        </button>
-        <button
-          className="button EditMenu__delete"
-          onClick={() => {
-            deleteCard(props.id);
-          }}
-        >
-          <i className="EditMenu__icon far fa-trash-alt"></i>Удалить
-        </button>
-      </div>
-    );
-  }
+  return (
+    <div className="EditMenu">
+      <button autoFocus className="button EditMenu__edit" onClick={editCard}>
+        <i className="EditMenu__icon fas fa-pencil-alt"></i>Редактировать
+      </button>
+      <button
+        className="button EditMenu__delete"
+        onClick={() => {
+          deleteCard(id);
+        }}
+      >
+        <i className="EditMenu__icon far fa-trash-alt"></i>Удалить
+      </button>
+    </div>
+  );
 }
