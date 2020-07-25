9725f9ff8d75f0fb0d83af237b5973e29cd14935
Evgeny Stepanov <evgenos@list.ru>
2020-07-25 23:21:37 +0300

    changed toogleMenu, connected to the store

--- src/components/Main/Giraffe.js
+++ src/components/Main/Giraffe.js
@@ -1,43 +1,44 @@
-import React, { useState } from 'react';
+import React from 'react';
 import './Giraffe.scss';
 import Motilda from '../../img/Motilda.png';
 import GiraffeEdit from './GiraffeEdit';
 import EditMenu from './EditMenu';
+import { StoreContext } from '../../service/store';
 
-export default function Giraffe(props) {
-  const [toggleMenu, setToggleMenu] = useState(false);
-  const [edit, setEdit] = useState(false);
+export default function Giraffe({ id, features }) {
+  const {
+    state: [state, setState],
+  } = React.useContext(StoreContext);
+  // console.log(state);
+  // let features = state.filter((i) => i.id === id)[0];
 
   function toggleEditMenu() {
-    setToggleMenu((toggleMenu) => !toggleMenu);
+    let arr = state.slice().map((item) => {
+      if (item.id === id) {
+        item.menuIsOpen = !item.menuIsOpen;
+        return item;
+      }
+      return item;
+    });
+    setState(arr);
   }
 
   let menu;
-  if (toggleMenu)
-    menu = (
-      <EditMenu
-        toggleMenu={toggleMenu}
-        setToggleMenu={setToggleMenu}
-        id={props.id}
-        edit={edit}
-        setEdit={setEdit}
-      />
-    );
+  if (features.menuIsOpen)
+    menu = <EditMenu toggleEditMenu={toggleEditMenu} id={id} />;
 
-  if (edit)
+  if (features.editMode)
     return (
       <GiraffeEdit
-        key={props.id}
-        id={props.id}
-        name={props.name}
-        gender={props.gender}
-        weight={props.weight}
-        height={props.height}
-        color={props.color}
-        diet={props.diet}
-        temper={props.temper}
-        edit={edit}
-        setEdit={setEdit}
+        key={features.id}
+        id={features.id}
+        name={features.name}
+        gender={features.gender}
+        weight={features.weight}
+        height={features.height}
+        color={features.color}
+        diet={features.diet}
+        temper={features.temper}
       />
     );
   return (
@@ -47,7 +48,7 @@ export default function Giraffe(props) {
       </div>
       {menu}
       <img className="foto" src={Motilda} alt="Motilda" />
-      <h2 className="name">{props.name}</h2>
+      <h2 className="name">{features.name}</h2>
       <div className="icons">
         <div className="icons__item">
           <i className="fas fa-venus-mars"></i>
@@ -60,28 +61,28 @@ export default function Giraffe(props) {
         </div>
       </div>
       <div className="features">
-        <div className="features__item features__venus">{props.gender}</div>
-        <div className="features__item features__scale">{props.weight}</div>
-        <div className="features__item features__ruler">{props.height}</div>
+        <div className="features__item features__venus">{features.gender}</div>
+        <div className="features__item features__scale">{features.weight}</div>
+        <div className="features__item features__ruler">{features.height}</div>
       </div>
       <div className="features-line">
         <div className="features-line__item">
           <span className="features-line__type">
             <strong>Цвет: </strong>
           </span>
-          <span className="features-line__value">{props.color}</span>
+          <span className="features-line__value">{features.color}</span>
         </div>
         <div className="features-line__item">
           <span className="features-line__type">
             <strong>Диета: </strong>
           </span>
-          <span className="features-line__value">{props.diet}</span>
+          <span className="features-line__value">{features.diet}</span>
         </div>
         <div className="features-line__item features-line__item--last">
           <span className="features-line__type">
             <strong>Характер: </strong>
           </span>
-          <span className="features-line__value">{props.temper}</span>
+          <span className="features-line__value">{features.temper}</span>
         </div>
       </div>
     </li>
