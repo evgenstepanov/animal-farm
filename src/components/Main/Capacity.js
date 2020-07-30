f1359cb2cd2e3504bd13f0a16324c8a53dad6e5a
Evgeny Stepanov <evgenos@list.ru>
2020-07-30 12:10:25 +0300

    processing the percentage line

--- src/components/Main/Capacity.js
+++ src/components/Main/Capacity.js
@@ -1,19 +1,28 @@
 import React from 'react';
 import './Capacity.scss';
+import { StoreContext } from '../../service/store';
 
 export default function Capacity() {
+  const {
+    state: [state],
+    capacity: [capacity],
+  } = React.useContext(StoreContext);
+
+  let width = ~~((state.length / capacity) * 100) + '%';
+  let style = { width: width };
+
   return (
     <div className="Capacity">
       <div className="Capacity__close">
         <i className="fas fa-times"></i>
       </div>
       <div className="Capacity__title-wrapper">
-        <span className="Capacity__percentage">75%</span>
+        <span className="Capacity__percentage">{width}</span>
         <h2 className="Capacity__title">Заполнение вольера</h2>
       </div>
       <div className="Capacity__line-wrapper">
         <div className="Capacity__line-container">
-          <div className="Capacity__line"></div>
+          <div className="Capacity__line" style={style}></div>
         </div>
         <div className="Capacity__info">Информация</div>
       </div>
