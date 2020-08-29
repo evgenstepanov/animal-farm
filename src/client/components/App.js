7c8672504293e97daade4c1e31a7f1ec0812a951
Evgeny Stepanov <evgenos@list.ru>
2020-08-29 22:57:11 +0300

    App component

---
+++ src/client/components/App.js
@@ -0,0 +1,16 @@
+import { hot } from 'react-hot-loader/root';
+import React from 'react';
+import './App.scss';
+import Footer from './Footer/Footer';
+import Main from './Main/Main';
+
+function App() {
+  return (
+    <div className="App-wrapper">
+      <Footer />
+      <Main />
+    </div>
+  );
+}
+
+export default hot(App);
