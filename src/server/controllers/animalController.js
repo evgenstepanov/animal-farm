e2c04309063c47b070f30575c344dc1f909af9e5
Evgeny Stepanov <evgenos@list.ru>
2020-08-29 21:55:24 +0300

    route controllers

---
+++ src/server/controllers/animalController.js
@@ -0,0 +1,57 @@
+const Animal = require('../models/animalModel');
+
+module.exports = {
+  getAnimals: async (req, res) => {
+    try {
+      const animals = await Animal.find();
+      res.json(animals);
+    } catch (err) {
+      res.json({ message: err });
+    }
+  },
+  postAnimal: async (req, res) => {
+    const animal = new Animal({
+      name: req.body.name,
+      weight: req.body.weight,
+      gender: req.body.gender,
+      height: req.body.height,
+      color: req.body.color,
+      diet: req.body.diet,
+      temper: req.body.temper,
+      image: req.body.image,
+    });
+    try {
+      const savedAnimal = await animal.save();
+      res.json(savedAnimal);
+    } catch (err) {
+      res.json({ message: err });
+    }
+  },
+  getAnimalById: async (req, res) => {
+    try {
+      const animal = await Animal.findById(req.params.id);
+      res.json(animal);
+    } catch (err) {
+      res.json({ message: err });
+    }
+  },
+  putAnimalById: async (req, res) => {
+    try {
+      const animal = await Animal.updateOne(
+        { _id: req.params.id },
+        { $set: req.body }
+      );
+      res.json(animal);
+    } catch (err) {
+      res.json({ message: err });
+    }
+  },
+  deleteAnimalById: async (req, res) => {
+    try {
+      const animal = await Animal.remove({ _id: req.params.id });
+      res.json(animal);
+    } catch (err) {
+      res.json({ message: err });
+    }
+  },
+};
