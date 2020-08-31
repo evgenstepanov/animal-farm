a269636f9846324b1143bc977e8fda960d804a89
Evgeny Stepanov <evgenos@list.ru>
2020-08-31 17:39:37 +0300

    added an empty form for adding a new card

--- src/client/components/Main/Cage.js
+++ src/client/components/Main/Cage.js
@@ -1,15 +1,29 @@
 import React from 'react';
 import Giraffe from './Giraffe';
+import GiraffeEdit from './GiraffeEdit';
 import './Cage.scss';
 import { StoreContext } from '../../context/store';
 
 export default function Cage() {
   const {
     state: [state],
+    newCard: [newCard],
   } = React.useContext(StoreContext);
+
+  let emptyCard = {
+    name: 'Имя',
+    gender: '-',
+    weight: 0,
+    height: 0,
+    color: '',
+    diet: '',
+    temper: '',
+    image: '',
+  };
 
   return (
     <ul className="Cage">
+      {newCard ? <GiraffeEdit features={emptyCard} /> : null}
       {state.map((features) => (
         <Giraffe key={features._id} features={features} />
       ))}
